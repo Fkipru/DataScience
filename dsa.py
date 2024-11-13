03af5b808707e4610a99312df982761cb7899d65
Fkipru <kiprutofestus55@gmail.com>
2024-11-13 10:52:35 +0300

    Create dsa.py

---
+++ dsa.py
@@ -0,0 +1,8 @@
+import numpy as np
+df2=pd.DataFrame({'A':1.,
+                  'B':pd.Timestamp('20010322'),
+                  'C':pd.Series(1,index=list(range(4)),dtype='float32'),
+                  'D':np.array([3]*4,dtype='int32'),
+                  'E':pd.Categorical(["test","train","test","train"]),
+                  'F':'foo'})
+df2
